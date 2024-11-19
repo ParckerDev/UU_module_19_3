7e71c714fb9e5e65aae651e1b4b1989eed7e87d7
Alexandr Shevchenko <djpestro@gmail.com>
2024-11-19 09:04:54 +0600

    create models Author and Post

--- NewsDaily/post/models.py
+++ NewsDaily/post/models.py
@@ -2,3 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Post(models.Model):
+    title = models.CharField(max_length=60)
+    content = models.TextField(max_length=1200)
+    author = models.ForeignKey(Author, related_name='author', on_delete=models.CASCADE)
+    create_date = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return self.title
